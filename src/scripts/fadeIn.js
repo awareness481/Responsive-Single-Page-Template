02b1092b4f1c783868e61ab6b04c0f7177fe7d3a
Jesse Jafa <jesse.jafa@gmail.com>
2018-07-19 12:28:46 +0300

    Code for fadein effect for thumbnails -- to be refactored

--- src/scripts/fadeIn.js
+++ src/scripts/fadeIn.js
@@ -1,12 +1,8 @@
 export function animationTrigger() {
-  let animated = false;
-  console.log('works');
 
   window.addEventListener('scroll', () => {
-    if (animated) {
-      return 1;
-    }
 
+    // Page2, three articles fade in
     if (window.scrollY > window.innerHeight / 5) {
       const fadeOne = document.getElementById('fadeInOne');
       const fadeTwo = document.getElementById('fadeInTwo');
@@ -15,8 +11,40 @@ export function animationTrigger() {
       fadeOne.classList.add('fadeOne');
       fadeTwo.classList.add('fadeTwo');
       fadeThree.classList.add('fadeThree');
+    }
 
-      animated = true;
+    // Page3, thumbnails fade in
+    // CODE TO BE REFACTORED BEFORE PUBLIC
+
+    if (window.scrollY > window.innerHeight + window.innerHeight / 2) {
+      const p1 = document.getElementById('p1');
+      p1.classList.add('fadeOne');
+
+      if (window.scrollY > window.innerHeight * 2) {
+        const p2 = document.getElementById('p2');
+        p2.classList.add('fadeTwo');
+
+        if (window.scrollY > window.innerHeight * 2.5) {
+          const p3 = document.getElementById('p3');
+          p3.classList.add('fadeTwo');
+
+          if (window.scrollY > window.innerHeight * 3) {
+            const p4 = document.getElementById('p4');
+            p4.classList.add('fadeTwo');
+
+            if (window.scrollY > window.innerHeight * 3.5) {
+              const p5 = document.getElementById('p5');
+              p5.classList.add('fadeTwo');
+
+              if (window.scrollY > window.innerHeight * 4) {
+                const p6 = document.getElementById('p6');
+                p6.classList.add('fadeTwo');
+              }
+            }
+          }
+        }
+      }
+      
     }
   })
 }

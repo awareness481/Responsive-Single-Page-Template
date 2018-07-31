c4dc5b88c3ae828bb6c90020d31553f6ab2a3c95
Jesse Jafa <jesse.jafa@gmail.com>
2018-07-31 15:44:55 +0300

    created script to get images pre-compilation

---
+++ get-images.sh
@@ -0,0 +1,4 @@
+wget https://www.dropbox.com/sh/486uh2zk1qsk6vz/AADIM-mCk27ObIP9zPJ-E6WRa?dl=1
+mkdir public/img
+unzip AADIM-mCk27ObIP9zPJ-E6WRa?dl=1 -d public/img
+rm -f AADIM-mCk27ObIP9zPJ-E6WRa?dl=1

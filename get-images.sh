a2cfb65c2856337d9e72f8f4048f3144ffa9c2ec
Jesse Jafa <jesse.jafa@gmail.com>
2018-08-02 10:41:56 +0300

    changed path to assets/

--- get-images.sh
+++ get-images.sh
@@ -1,4 +1,4 @@
 wget https://www.dropbox.com/sh/486uh2zk1qsk6vz/AADIM-mCk27ObIP9zPJ-E6WRa?dl=1
-mkdir public/img
-unzip AADIM-mCk27ObIP9zPJ-E6WRa?dl=1 -d public/img
+mkdir assets
+unzip AADIM-mCk27ObIP9zPJ-E6WRa?dl=1 -d assets
 rm -f AADIM-mCk27ObIP9zPJ-E6WRa?dl=1
